4e3b11d291e51ff18ace9da5e5d0e6e04cc78d2e
wmichael <vve11ermichae1@gmail.com>
2020-06-27 18:49:21 +0100

    Seperated this repo into frontend

--- backend/src/Models/receipt.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const ReceiptSchema = require('../Schemas/receipt.schema');
-
-const ReceiptModel = mongoose.model('Receipt', ReceiptSchema);
-
-module.exports = ReceiptModel;
